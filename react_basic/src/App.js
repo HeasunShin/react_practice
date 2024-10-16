a95e0f67372b36c3d64c0c86c79d9565e2623fb6
SHIN HEASUN <shyins9191@gmail.com>
2024-10-16 16:42:27 +0900

    test

---
+++ react_basic/src/App.js
@@ -0,0 +1,13 @@
+// 컴포넌트 만들기
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello React</h2>
+      </div>
+    )
+  }
+}
+
