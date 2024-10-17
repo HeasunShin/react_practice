304f65d79aa7e71fdded21778248b5cfcd6accd1
SHIN HEASUN <shyins9191@gmail.com>
2024-10-17 09:58:11 +0900

    react_basic_1 브랜치 생성

--- react_basic/src/App.js
+++ react_basic/src/App.js
@@ -1,11 +1,53 @@
 // 컴포넌트 만들기
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+class Myheader extends Component {
+  render() {
+    return (
+      <header>
+        <h1 className='logo'>React</h1>
+        <p>Single Page Application</p>
+      </header>
+    )
+  }
+}
+
+class Mynav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="">HTML</a></li>
+          <li><a href="">CSS</a></li>
+          <li><a href="">Javascript</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+class Myarticle extends Component {
+  render() {
+    return (
+      <section>
+        <article>
+          <h2>HTML</h2>
+          <p>Hypertext Markup Language</p>
+        </article>
+      </section>
+    )
+  }
+}
+
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h2>Hello React - test</h2>
+      <div className='App'>
+        <Myheader/>
+        <Mynav/>
+        <Myarticle/>
       </div>
     )
   }
