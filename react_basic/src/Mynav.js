85da4c097810cd52af2fc3688d25b5ec66e1caaf
SHIN HEASUN <shyins9191@gmail.com>
2024-10-22 10:47:53 +0900

    241022

--- react_basic/src/Mynav.js
+++ react_basic/src/Mynav.js
@@ -3,6 +3,29 @@ import React, { Component } from 'react'
 export default class Mynav extends Component {
     render() {
         var lists = [];
+
+        // 리팩토링(기존에 넘어온 값을 새로운 변수에 다시 담아주는 것)
+        var data = this.props.data;
+
+        // while 반복문
+        // var  i = 0;
+        // while(조건){
+        //     반복할일
+        //     증감
+        // }
+        // for 반복문
+        // for(i=0; i<3; i++){
+            
+        // }
+
+        var  i = 0;
+        while(i<data.length){
+            lists.push(<li key={data[i].id}><a href="">{data[i].title}</a></li>);
+            // i++;
+            // i = i+1;
+            i+=1;
+        }
+
         return (
             <nav>
             <ul>
